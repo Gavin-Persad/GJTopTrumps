093932384a084b5d92fc118d075b18fd6f15585f
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-07-25 13:56:35 +0100

    tests copied accross

--- src/app/helpers.test.ts
+++ src/app/helpers.test.ts
@@ -1,7 +1,15 @@
-import { sum } from './helpers';
+import { sum } from "./helpers";
+import { disp } from "./helpers";
 
-test('sum adds two numbers correctly', () => {
+test("sum adds two numbers correctly", () => {
   expect(sum(1, 2)).toBe(3);
   expect(sum(-5, 10)).toBe(5);
   expect(sum(0, 0)).toBe(0);
+});
+
+test("returns names array correctly", () => {
+  const nameArray: Array<string> = ["Mary", "Tom", "Jack", "Jill"];
+  it("should return list of names", () => {
+    expect(disp(nameArray).toBe());
+  });
 });
--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,3 +1,28 @@
-export function sum (a: number, b: number): number {
-  return a + b
-};
+export function sum(a: number, b: number): number {
+  return a + b;
+}
+
+export function shuffleAndSplitCards(data: []) {
+  const shuffledData = [...data].sort(() => Math.random() - 0.5);
+
+  const player: [] = [];
+  const computer: [] = [];
+
+  for (let i = 0; i < shuffledData.length; i++) {
+    if (i % 2 === 0) {
+      player.push(shuffledData[i]);
+    } else {
+      computer.push(shuffledData[i]);
+    }
+  }
+  return { playerCards: player, computerCards: computer };
+}
+
+export function disp(arr_names: string[]) {
+  var names: string[] = [];
+
+  for (var i = 0; i < arr_names.length; i++) {
+    names.push(arr_names[i]);
+  }
+  return names;
+}
