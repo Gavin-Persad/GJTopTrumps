f24c61994f746c0295b277e6a9b76955d17d83d2
Gavin-Persad <gavinapersad@gmail.com>
2024-08-08 12:45:18 +0100

    info and title in place

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,9 @@
 
 const Header = () => {
   return (
-    <header className="bg-blue-700">
-      <h1>Top Trumps</h1>
+    <header className="flex items-center bg-blue-700 justify-between p-4">
+      <h1 className="">Top Trumps</h1>
+      <button className="rounded-full bg-white h-8 w-8">ℹ️</button>
     </header>
   );
 };
