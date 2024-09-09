2c125f512d92ccae06c5800d07ff4d2c371b9e98
Gavin-Persad <gavinapersad@gmail.com>
2024-09-09 17:47:03 +0100

    info button fixed

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -15,7 +15,7 @@ const toggleHowToPlay = () => {
     <>
       <header className="flex items-center bg-blue-700 justify-between p-4">
         <h1 className="">Top Trumps</h1>
-        <button className="rounded-full bg-white h-8 w-8" onClick={toggleHowToPlay}>ℹ️</button>
+        <button className="rounded-full bg-blue-700 text-white  border-2 border-white-500 rounded  h-8 w-8" onClick={toggleHowToPlay}>ℹ️</button>
       </header>
       {howToPlay && (
         <div className="bg-blue-700 text-white p-4">
